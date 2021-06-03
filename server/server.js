cb2be998ea8ad20f7500bdc3acb1b6beacc97005
Jelvani <aj654@rutgers.edu>
2021-06-03 17:27:52 -0400

    should have done this earlier

---
+++ server/server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const mc = require("mongodb").MongoClient;
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+
+
+var wordFilter = require('bad-words');
+filter = new wordFilter();
+
+const port = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+let db = null;
+let collection = null;
+
+
+app.get("/", (req, res) => {
+    collection.find().project({ company: 1, review: 1, rating: 1, date: 1 })
+        .toArray(function (err, result) {
+            if (err) throw err;
+            //TODO: optimize this process, maybe dont check every time.
+            //we filter each company name and review
+
+            result.forEach(function (entry, i) {
+                if (!(entry.company || entry.review || entry.date || entry.rating)) {
+                    return; //same as 'continue' in normal loop
+                }
+                result[i].company = filter.clean(entry.company);
+                result[i].review = filter.clean(entry.review);
+                console.log(entry.date.toDateString());
+                result[i].date = entry.date.toDateString();
+            });
+            res.json(result);
+        });
+});
+
+app.post("/add", (req, res) => {
+    let entry = {
+        company: req.body.company,
+        review: req.body.review,
+        rating: req.body.rating,
+        date: new Date(),
+        ip: req.connection.remoteAddress
+    };
+
+    collection.insertOne(entry)
+        .then(result => {
+            console.log("Inserted entry!");
+        })
+        .catch(error => console.error(error));
+});
+
+app.listen(port, () => {
+    mc.connect(uri).then(client => {
+        console.log("Connected to Databse");
+        db = client.db('userData');
+        collection = db.collection("reviews");
+    })
+        .catch(error => console.error(error));
+    console.log(`Server is running on port: ${port}`);
+});
